3893e4c561d7dd6031e64946ac7d29a5e204052e
dension <like_cosmos@163.com>
2023-09-18 16:53:40 +0800

    修改config_v1.3

--- removedir.py
+++ removedir.py
@@ -3,26 +3,27 @@ import os
 import config
 
 
-def remove_empty_files():
+def remove_empty_files(fil):
     conf = config.getInstance()
     folder_path = conf.source_folder()
     for root, dirs, files in os.walk(folder_path):
 
         for file in files:
-
             file_path = os.path.join(root, file)
-
-            if os.path.getsize(file_path) == 0:
-
-                pass
-
-                # os.remove(file_path)
-
-            print(os.path.getsize(file_path))
-
-
+            if os.path.getsize(file_path) < 51200:
+                try:
+                    os.remove(file_path)
+                except:
+                    fil.write(os.path.getsize(file_path))
 
 
 
 if __name__ == '__main__':
-    remove_empty_files()
+
+    if not os.path.isfile('./test.txt'):
+        file = open('./test.txt', 'w')
+    else:
+        file = open('./test.txt', 'a')
+
+    remove_empty_files(file)
+    file.close()
