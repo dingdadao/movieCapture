f5540ad2c24a8af9f76b1c70a9f3e4ff60d01bf8
dension <like_cosmos@163.com>
2023-10-08 17:15:08 +0800

    增加一个删除v1.4

--- test.py
+++ test.py
@@ -13,16 +13,30 @@ def main():
 
     source = Path(faileds).resolve()
     # print(source,"faileds-------")
+    deletelist = []
+
     for full_name in source.glob(r'**/*'):
         is_sym = full_name.is_symlink()
         movie_size = 0 if is_sym else full_name.stat().st_size
 
         if 0 < movie_size < 125829120:
-            movie_path = str(os.path.join(full_name))
-            try:
-                os.remove(movie_path)
-            except:
-                print("删除失败了奥",movie_path)
+            deletelist.append(full_name)
 
+    delete_cont = len(deletelist)
+    if delete_cont > 0:
+        print("一共发现"+str()+"条需要删除的数据")
+    else:
+        print("没有需要删除的数据")
+        return
+
+
+    for _ in deletelist:
+        movie_path = str(os.path.join(_))
+        try:
+            os.remove(movie_path)
+        except:
+            print("删除失败了奥", movie_path)
+
+    print("删除结束")
 
 main()
