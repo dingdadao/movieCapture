e4fdb5f3934f7a77db743a3ba1c409e80131819a
dension <like_cosmos@163.com>
2023-10-08 17:05:00 +0800

    增加一个删除v1.3

--- test.py
+++ test.py
@@ -7,15 +7,22 @@ def main():
     print("开始了")
     conf = config.getInstance()
     faileds = conf.failed_folder()
-    print(faileds,"faileds-----------")
+    # print(faileds,"faileds-----------")
     if not isinstance(faileds, str) or faileds == '':
         faileds = os.path.abspath(".")
 
     source = Path(faileds).resolve()
-    print(source,"faileds-------")
+    # print(source,"faileds-------")
     for full_name in source.glob(r'**/*'):
-        print(full_name)
+        is_sym = full_name.is_symlink()
+        movie_size = 0 if is_sym else full_name.stat().st_size
 
+        if 0 < movie_size < 125829120:
+            movie_path = str(os.path.join(full_name))
+            try:
+                os.remove(movie_path)
+            except:
+                print("删除失败了奥",movie_path)
 
 
 main()
