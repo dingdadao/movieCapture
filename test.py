d3591fac42e10ec6026d601c24a469d6827347d7
dension <like_cosmos@163.com>
2023-10-08 16:59:44 +0800

    增加一个删除v1.2

--- test.py
+++ test.py
@@ -7,11 +7,12 @@ def main():
     print("开始了")
     conf = config.getInstance()
     faileds = conf.failed_folder()
+    print(faileds,"faileds-----------")
     if not isinstance(faileds, str) or faileds == '':
         faileds = os.path.abspath(".")
 
     source = Path(faileds).resolve()
-
+    print(source,"faileds-------")
     for full_name in source.glob(r'**/*'):
         print(full_name)
 
