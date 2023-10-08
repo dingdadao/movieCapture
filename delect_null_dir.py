ab41b6072b3ad8a40a8b423acdb4824526e7a556
dension <like_cosmos@163.com>
2023-10-08 17:25:43 +0800

    删除空文件夹

---
+++ delect_null_dir.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+import config
+
+
+def main():
+    print("开始了")
+    conf = config.getInstance()
+    sources = conf.source_folder()
+    # print(faileds,"faileds-----------")
+    if not isinstance(sources, str) or sources == '':
+        sources = os.path.abspath(".")
+
+    sources = Path(sources).resolve()
+    for _ in sources:
+        patha = str(os.path.join(_))
+        if os.path.isdir(patha):
+            print(patha)
+            if os.stat().st_size <= 0:
+                print(patha,"小于0")
+
