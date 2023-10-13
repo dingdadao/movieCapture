1f7f93d4d920215669b587ed1b65cb8b0735b53e
dension <like_cosmos@163.com>
2023-10-13 16:24:10 +0800

    修正下载太快了，直接迁移文件

--- mov_Movie.py
+++ mov_Movie.py
@@ -18,20 +18,19 @@ def mov_Movie():
 
     source = Path(source_folder).resolve()
     for full_name in source.glob(r'**/*'):
-        is_sym = full_name.is_symlink()
-        # 调试用0字节样本允许通过，去除小于120MB的广告'苍老师强力推荐.mp4'(102.2MB)'黑道总裁.mp4'(98.4MB)'有趣的妹子激情表演.MP4'(95MB)'有趣的臺灣妹妹直播.mp4'(15.1MB)
-        movie_size = 0 if is_sym else full_name.stat().st_size  # 同上 符号链接不取stat()及st_size，直接赋0跳过小视频检测
-        if 0 < movie_size < 82428800:  # 1024*1024*120=125829120
-            try:
-                os.remove(full_name)
-                print("删除了小于80m的文件，节约空间")
-            except:
-                print("删除失败了{0}".format(full_name))
-
         if not full_name.suffix.lower() in file_type:
             continue
         else:
             print(full_name)
+            is_sym = full_name.is_symlink()
+            # 调试用0字节样本允许通过，去除小于120MB的广告'苍老师强力推荐.mp4'(102.2MB)'黑道总裁.mp4'(98.4MB)'有趣的妹子激情表演.MP4'(95MB)'有趣的臺灣妹妹直播.mp4'(15.1MB)
+            movie_size = 0 if is_sym else full_name.stat().st_size  # 同上 符号链接不取stat()及st_size，直接赋0跳过小视频检测
+            if 0 < movie_size < 82428800:  # 1024*1024*120=125829120
+                try:
+                    os.remove(full_name)
+                    print("删除了小于80m的文件，节约空间")
+                except:
+                    print("删除失败了{0}".format(full_name))
             shutil.move(full_name, check_folder)
 
 
