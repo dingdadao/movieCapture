2e2e609caa10d61471bed126e8593efba273541e
dension <like_cosmos@163.com>
2024-11-20 09:18:03 +0800

    代理规则v9

--- getnumber/searchJavdbDowload.py
+++ getnumber/searchJavdbDowload.py
@@ -137,9 +137,9 @@ if __name__ == '__main__':
     # "https://javdb366.com/directors/4Dd6"1
     # "https://javdb366.com/video_codes/NSSTH"1
 
-    # url ="https://javdb366.com/search?f=all&q=nsps"
-    url=None
+    url ="https://javdb366.com/series/4d0J"
+    # url=None
 
 
-    Javdblist = get_Javdb(keyword='nsps',sb=1, ifor='download', max=21, urls=url)
+    Javdblist = get_Javdb(keyword='sdnt',sb=1, ifor='download', max=2, urls=url)
 
